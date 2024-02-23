4567d9badfdd9504e4e9ffd016cd87de3bef1601
xiay <9046831+superxiay@user.noreply.gitee.com>
2024-02-23 13:40:20 +0800

    modify astra_camera

--- src/list_devices_node.cpp
+++ src/list_devices_node.cpp
@@ -28,6 +28,7 @@ void DeviceConnectedCallback(const openni::DeviceInfo *device_info) {
 }
 
 int main() {
+  char **argv;
   google::InitGoogleLogging(argv[0]);
   google::SetStderrLogging(google::GLOG_INFO);
   FLAGS_colorlogtostderr = true;

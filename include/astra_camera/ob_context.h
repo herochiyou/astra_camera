b2fed2f130914dfdfc33737acf1926be151cef92
xiay <9046831+superxiay@user.noreply.gitee.com>
2024-02-25 15:50:22 +0800

    modify astra_camera

--- include/astra_camera/ob_context.h
+++ include/astra_camera/ob_context.h
@@ -10,37 +10,39 @@
 /*                                                                        */
 /**************************************************************************/
 #pragma once
+#include <glog/logging.h>
+#include <mutex>
 #include <openni2/OpenNI.h>
 #include <ros/ros.h>
-#include <mutex>
 
 namespace astra_camera {
 
-using DeviceConnectedCb = std::function<void(const openni::DeviceInfo*)>;
+using DeviceConnectedCb = std::function<void(const openni::DeviceInfo *)>;
 
-using DeviceDisconnectedCb = std::function<void(const openni::DeviceInfo*)>;
+using DeviceDisconnectedCb = std::function<void(const openni::DeviceInfo *)>;
 
 class Context : public openni::OpenNI::DeviceConnectedListener,
                 public openni::OpenNI::DeviceDisconnectedListener,
                 public openni::OpenNI::DeviceStateChangedListener {
- public:
+  public:
   explicit Context(DeviceDisconnectedCb device_disconnected_cb);
 
   ~Context() override;
 
-  void onDeviceStateChanged(const openni::DeviceInfo* pInfo, openni::DeviceState state) override;
+  void onDeviceStateChanged(const openni::DeviceInfo *pInfo,
+                            openni::DeviceState state) override;
 
-  void onDeviceConnected(const openni::DeviceInfo* pInfo) override;
+  void onDeviceConnected(const openni::DeviceInfo *pInfo) override;
 
-  void onDeviceDisconnected(const openni::DeviceInfo* pInfo) override;
+  void onDeviceDisconnected(const openni::DeviceInfo *pInfo) override;
 
   std::vector<openni::DeviceInfo> queryDeviceList();
 
- private:
+  private:
   std::recursive_mutex mutex_;
   bool first_time_query_ = true;
   DeviceDisconnectedCb device_disconnected_cb_;
   std::map<std::string, openni::DeviceInfo> device_info_list_;
 };
 
-}  // namespace astra_camera
+} // namespace astra_camera

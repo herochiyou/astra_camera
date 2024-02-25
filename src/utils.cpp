787747bebdaae8c5f76badc635ffa5a6e6a31e2b
xiay <9046831+superxiay@user.noreply.gitee.com>
2024-02-25 15:57:08 +0800

    modify astra_camera

--- src/utils.cpp
+++ src/utils.cpp
@@ -19,36 +19,40 @@
 
 namespace astra_camera {
 
-bool operator==(const openni::VideoMode& lhs, const openni::VideoMode& rhs) {
+bool operator==(const openni::VideoMode &lhs, const openni::VideoMode &rhs) {
   return lhs.getResolutionY() == rhs.getResolutionY() &&
-         lhs.getResolutionX() == rhs.getResolutionX() && lhs.getFps() == rhs.getFps() &&
+         lhs.getResolutionX() == rhs.getResolutionX() &&
+         lhs.getFps() == rhs.getFps() &&
          lhs.getPixelFormat() == rhs.getPixelFormat();
 }
 
-bool operator!=(const openni::VideoMode& lhs, const openni::VideoMode& rhs) {
+bool operator!=(const openni::VideoMode &lhs, const openni::VideoMode &rhs) {
   return !(lhs == rhs);
 }
 
-std::ostream& operator<<(std::ostream& os, const openni::VideoMode& video_mode) {
-  os << "Resolution :" << video_mode.getResolutionX() << "x" << video_mode.getResolutionY() << "@"
-     << video_mode.getFps() << "Hz" << std::endl
+std::ostream &operator<<(std::ostream &os,
+                         const openni::VideoMode &video_mode) {
+  os << "Resolution :" << video_mode.getResolutionX() << "x"
+     << video_mode.getResolutionY() << "@" << video_mode.getFps() << "Hz"
+     << std::endl
      << "format " << PixelFormatToString(video_mode.getPixelFormat());
   return os;
 }
 
-tf2::Quaternion rotationMatrixToQuaternion(const std::vector<float>& rotation) {
+tf2::Quaternion rotationMatrixToQuaternion(const std::vector<float> &rotation) {
   CHECK_EQ(rotation.size(), 9u);
   Eigen::Matrix3f m;
   // We need to be careful about the order, as RS2 rotation matrix is
   // column-major, while Eigen::Matrix3f expects row-major.
-  m << rotation[0], rotation[3], rotation[6], rotation[1], rotation[4], rotation[7], rotation[2],
-      rotation[5], rotation[8];
+  m << rotation[0], rotation[3], rotation[6], rotation[1], rotation[4],
+      rotation[7], rotation[2], rotation[5], rotation[8];
   Eigen::Quaternionf q(m);
   return {q.x(), q.y(), q.z(), q.w()};
 }
 
-Extrinsics obExtrinsicsToMsg(const std::vector<float>& rotation,
-                             const std::vector<float>& transition, const std::string& frame_id) {
+Extrinsics obExtrinsicsToMsg(const std::vector<float> &rotation,
+                             const std::vector<float> &transition,
+                             const std::string &frame_id) {
   CHECK_EQ(rotation.size(), 9u);
   CHECK_EQ(transition.size(), 3u);
   Extrinsics msg;
@@ -63,7 +67,7 @@ Extrinsics obExtrinsicsToMsg(const std::vector<float>& rotation,
   return msg;
 }
 
-bool isValidCameraParams(const OBCameraParams& params) {
+bool isValidCameraParams(const OBCameraParams &params) {
   if (std::isnan(params.l_intr_p[0]) || std::isnan(params.l_intr_p[1]) ||
       std::isnan(params.l_intr_p[2]) || std::isnan(params.l_intr_p[3])) {
     return false;
@@ -71,8 +75,8 @@ bool isValidCameraParams(const OBCameraParams& params) {
   return true;
 }
 
-void cameraParameterPrinter(const std::vector<float>& rotation,
-                            const std::vector<float>& transition) {
+void cameraParameterPrinter(const std::vector<float> &rotation,
+                            const std::vector<float> &transition) {
   CHECK_EQ(rotation.size(), 9u);
   CHECK_EQ(transition.size(), 3u);
   std::cout << "Rotation: " << std::endl;
@@ -87,39 +91,40 @@ void cameraParameterPrinter(const std::vector<float>& rotation,
   std::cout << std::endl;
 }
 
-std::string PixelFormatToString(const openni::PixelFormat& format) {
+std::string PixelFormatToString(const openni::PixelFormat &format) {
   switch (format) {
-    case openni::PIXEL_FORMAT_DEPTH_1_MM:
-      return "PIXEL_FORMAT_DEPTH_1_MM";
-    case openni::PIXEL_FORMAT_DEPTH_100_UM:
-      return "PIXEL_FORMAT_DEPTH_100_UM";
-    case openni::PIXEL_FORMAT_SHIFT_9_2:
-      return "PIXEL_FORMAT_SHIFT_9_2";
-    case openni::PIXEL_FORMAT_SHIFT_9_3:
-      return "PIXEL_FORMAT_SHIFT_9_3";
-    case openni::PIXEL_FORMAT_RGB888:
-      return "PIXEL_FORMAT_RGB888";
-    case openni::PIXEL_FORMAT_YUV422:
-      return "PIXEL_FORMAT_YUV422";
-    case openni::PIXEL_FORMAT_GRAY8:
-      return "PIXEL_FORMAT_GRAY8";
-    case openni::PIXEL_FORMAT_GRAY16:
-      return "PIXEL_FORMAT_GRAY16";
-    case openni::PIXEL_FORMAT_JPEG:
-      return "PIXEL_FORMAT_JPEG";
-    case openni::PIXEL_FORMAT_YUYV:
-      return "PIXEL_FORMAT_YUYV";
-    case openni::PIXEL_FORMAT_LOG:
-      return "PIXEL_FORMAT_LOG";
+  case openni::PIXEL_FORMAT_DEPTH_1_MM:
+    return "PIXEL_FORMAT_DEPTH_1_MM";
+  case openni::PIXEL_FORMAT_DEPTH_100_UM:
+    return "PIXEL_FORMAT_DEPTH_100_UM";
+  case openni::PIXEL_FORMAT_SHIFT_9_2:
+    return "PIXEL_FORMAT_SHIFT_9_2";
+  case openni::PIXEL_FORMAT_SHIFT_9_3:
+    return "PIXEL_FORMAT_SHIFT_9_3";
+  case openni::PIXEL_FORMAT_RGB888:
+    return "PIXEL_FORMAT_RGB888";
+  case openni::PIXEL_FORMAT_YUV422:
+    return "PIXEL_FORMAT_YUV422";
+  case openni::PIXEL_FORMAT_GRAY8:
+    return "PIXEL_FORMAT_GRAY8";
+  case openni::PIXEL_FORMAT_GRAY16:
+    return "PIXEL_FORMAT_GRAY16";
+  case openni::PIXEL_FORMAT_JPEG:
+    return "PIXEL_FORMAT_JPEG";
+  case openni::PIXEL_FORMAT_YUYV:
+    return "PIXEL_FORMAT_YUYV";
+    // case openni::PIXEL_FORMAT_LOG:
+    // return "PIXEL_FORMAT_LOG";
   }
   return "Unknown";
 }
-void savePointToPly(sensor_msgs::PointCloud2::Ptr cloud, const std::string& filename) {
+void savePointToPly(sensor_msgs::PointCloud2::Ptr cloud,
+                    const std::string &filename) {
   sensor_msgs::PointCloud out_point_cloud;
   sensor_msgs::convertPointCloud2ToPointCloud(*cloud, out_point_cloud);
   size_t point_size = 0;
-  FILE* fp = fopen(filename.c_str(), "wb+");
-  for (auto& point : out_point_cloud.points) {
+  FILE *fp = fopen(filename.c_str(), "wb+");
+  for (auto &point : out_point_cloud.points) {
     if (!std::isnan(point.x) && !std::isnan(point.y) && !std::isnan(point.z)) {
       point_size++;
     }
@@ -132,7 +137,7 @@ void savePointToPly(sensor_msgs::PointCloud2::Ptr cloud, const std::string& file
   fprintf(fp, "property float y\n");
   fprintf(fp, "property float z\n");
   fprintf(fp, "end_header\n");
-  for (const auto& point : out_point_cloud.points) {
+  for (const auto &point : out_point_cloud.points) {
     if (!std::isnan(point.x) && !std::isnan(point.y) && !std::isnan(point.z)) {
       fprintf(fp, "%.3f %.3f %.3f\n", point.x, point.y, point.z);
     }
@@ -141,7 +146,8 @@ void savePointToPly(sensor_msgs::PointCloud2::Ptr cloud, const std::string& file
   fclose(fp);
 }
 
-void saveRGBPointToPly(sensor_msgs::PointCloud2::Ptr cloud, const std::string& filename) {
+void saveRGBPointToPly(sensor_msgs::PointCloud2::Ptr cloud,
+                       const std::string &filename) {
   sensor_msgs::PointCloud2Iterator<float> iter_x(*cloud, "x");
   sensor_msgs::PointCloud2Iterator<float> iter_y(*cloud, "y");
   sensor_msgs::PointCloud2Iterator<float> iter_z(*cloud, "z");
@@ -172,7 +178,7 @@ void saveRGBPointToPly(sensor_msgs::PointCloud2::Ptr cloud, const std::string& f
     ++iter_b;
   }
   point_size = points.size();
-  FILE* fp = fopen(filename.c_str(), "wb+");
+  FILE *fp = fopen(filename.c_str(), "wb+");
 
   ROS_INFO_STREAM("Point size: " << point_size);
   fprintf(fp, "ply\n");
@@ -187,14 +193,14 @@ void saveRGBPointToPly(sensor_msgs::PointCloud2::Ptr cloud, const std::string& f
   fprintf(fp, "end_header\n");
 
   for (size_t i = 0; i < point_size; ++i) {
-    fprintf(fp, "%.3f %.3f %.3f %d %d %d\n", points[i].x, points[i].y, points[i].z, rgb[i][0],
-            rgb[i][1], rgb[i][2]);
+    fprintf(fp, "%.3f %.3f %.3f %d %d %d\n", points[i].x, points[i].y,
+            points[i].z, rgb[i][0], rgb[i][1], rgb[i][2]);
   }
   fflush(fp);
   fclose(fp);
 }
 
-MultiDeviceSyncMode getMultiDeviceSyncMode(const std::string& mode) {
+MultiDeviceSyncMode getMultiDeviceSyncMode(const std::string &mode) {
   if (mode == "none") {
     return MultiDeviceSyncMode::None;
   } else if (mode == "master" || mode == "main") {
@@ -206,5 +212,4 @@ MultiDeviceSyncMode getMultiDeviceSyncMode(const std::string& mode) {
   }
 }
 
-
-}  // namespace astra_camera
+} // namespace astra_camera

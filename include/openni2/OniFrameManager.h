e3865e6c93b4155183489600f645361adcb27a4a
xiay <9046831+superxiay@user.noreply.gitee.com>
2024-02-25 16:30:03 +0800

    modify astra_camera

--- include/openni2/OniFrameManager.h
+++ include/openni2/OniFrameManager.h
@@ -1,7 +1,7 @@
 #ifndef _ONI_FRAME_MANAGER_H_
 #define _ONI_FRAME_MANAGER_H_
 
-#include <OniCTypes.h>
+#include "OniCTypes.h"
 #include "OniCommon.h"
 #include <XnOS.h>
 #include <XnPool.h>
@@ -9,30 +9,31 @@
 ONI_NAMESPACE_IMPLEMENTATION_BEGIN
 
 struct OniFrameInternal;
-typedef void (ONI_CALLBACK_TYPE* BackToPoolFuncPtr)(struct OniFrameInternal* pBuffer, void* pCookie);
-typedef void (ONI_CALLBACK_TYPE* FreeBufferFuncPtr)(void* data, void* pCookie);
+typedef void(ONI_CALLBACK_TYPE *BackToPoolFuncPtr)(
+    struct OniFrameInternal *pBuffer, void *pCookie);
+typedef void(ONI_CALLBACK_TYPE *FreeBufferFuncPtr)(void *data, void *pCookie);
 
-struct OniFrameInternal : public OniFrame
-{
-	int refCount;
-	BackToPoolFuncPtr backToPoolFunc; // callback function to be called when frame reached zero refs and returned to pool
-	void* backToPoolFuncCookie;
-	FreeBufferFuncPtr freeBufferFunc; // callback function for freeing the frame buffer
-	void* freeBufferFuncCookie;
+struct OniFrameInternal : public OniFrame {
+  int refCount;
+  BackToPoolFuncPtr backToPoolFunc; // callback function to be called when frame
+                                    // reached zero refs and returned to pool
+  void *backToPoolFuncCookie;
+  FreeBufferFuncPtr
+      freeBufferFunc; // callback function for freeing the frame buffer
+  void *freeBufferFuncCookie;
 };
 
-class FrameManager
-{
-public:
-	FrameManager();
-	~FrameManager();
+class FrameManager {
+  public:
+  FrameManager();
+  ~FrameManager();
 
-	OniFrameInternal* acquireFrame();
-	void addRef(OniFrame* pFrame);
-	void release(OniFrame* pFrame);
+  OniFrameInternal *acquireFrame();
+  void addRef(OniFrame *pFrame);
+  void release(OniFrame *pFrame);
 
-private:
-	xnl::Pool<OniFrameInternal> m_frames;
+  private:
+  xnl::Pool<OniFrameInternal> m_frames;
 };
 
 ONI_NAMESPACE_IMPLEMENTATION_END
